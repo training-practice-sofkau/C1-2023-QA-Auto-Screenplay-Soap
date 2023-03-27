b5dd1d3edebe750ed5ccf79f706c10d888611351
MiguelMendoza22 <jesusmiguelmm@ufps.edu.co>
2023-03-27 16:24:11 -0500

    se agrega la automatización de la multiplicación de la calculadora

---
+++ TestServicesSoap/src/test/java/com/sofkau/runners/MultiplyTest.java
@@ -0,0 +1,20 @@
+package com.sofkau.runners;
+
+import com.sofkau.setup.ApiSetUp;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.junit.CucumberOptions;
+import net.serenitybdd.cucumber.CucumberWithSerenity;
+import org.junit.runner.RunWith;
+
+@RunWith(CucumberWithSerenity.class)
+@CucumberOptions(
+        snippets = CucumberOptions.SnippetType.CAMELCASE,
+        features = {"src/test/resources/features/multiply.feature"},
+        glue = {"com.sofkau.stepdefinitions"},
+        tags = {}
+)
+public class MultiplyTest  {
+
+}
--- TestServicesSoap/src/test/java/com/sofkau/stepdefinitions/MultiplyStepDefinitions.java
+++ TestServicesSoap/src/test/java/com/sofkau/stepdefinitions/MultiplyStepDefinitions.java
@@ -12,21 +12,22 @@ import org.junit.jupiter.api.Assertions;
 import java.nio.charset.StandardCharsets;
 
 import static com.sofkau.models.Headers.headers;
+import static com.sofkau.models.HeadersMultiply.headersMultiply;
 import static com.sofkau.questions.ResponseSoap.responseSoap;
 import static com.sofkau.tasks.DoPostSoap.doPostSoap;
 import static com.sofkau.utils.ManageFile.readFile;
-import static com.sofkau.utils.Path_ZipCode.*;
+import static com.sofkau.utils.Path_Calculator.*;
 import static net.serenitybdd.screenplay.GivenWhenThen.seeThat;
 import static net.serenitybdd.screenplay.rest.questions.ResponseConsequence.seeThatResponse;
 import static org.hamcrest.CoreMatchers.containsString;
 
-public class LatLongZipCodeStepDefinitions extends ApiSetUp {
+public class MultiplyStepDefinitions extends ApiSetUp {
     String body;
-    private static final Logger LOGGER = Logger.getLogger(LatLongZipCodeStepDefinitions.class);
-    @Given("a user knows the postal code of a place")
-    public void aUserKnowsThePostalCodeOfAPlace() {
+    private static final Logger LOGGER = Logger.getLogger(CapitalStepDefinitions.class);
+    @Given("a user needs to perform a multiplication")
+    public void aUserNeedsToPerformAMultiplication() {
         try {
-            setUp(SOAP_lAT_LONG_LIST_BASE_URL.getValue());
+            setUp(CALCULATOR_BASE_URL.getValue());
             LOGGER.info("INICIA LA AUTOMATIZACION");
             loadBody();
         } catch (Exception e) {
@@ -36,13 +37,13 @@ public class LatLongZipCodeStepDefinitions extends ApiSetUp {
         }
     }
 
-    @When("the user sends the request with the zip code to the API")
-    public void theUserSendsTheRequestWithTheZipCodeToTheAPI() {
+    @When("the user sends the request with two integers to the API")
+    public void theUserSendsTheRequestWithTwoIntegersToTheAPI() {
         try {
             actor.attemptsTo(
                     doPostSoap()
-                            .andTheResource(RESOURCE_LAT_LONG.getValue())
-                            .withTheHeaders(headers().getHeadersCollection())
+                            .andTheResource(RESOURCE_MULTIPLY.getValue())
+                            .withTheHeaders(headersMultiply().getHeadersCollection())
                             .andTheBody(body)
             );
             LOGGER.info("Realiza la peticion");
@@ -53,15 +54,15 @@ public class LatLongZipCodeStepDefinitions extends ApiSetUp {
         }
     }
 
-    @Then("the user gets the latitude and longitude of the place")
-    public void theUserGetsTheLatitudeAndLongitudeOfThePlace() {
+    @Then("he user gets the result of the multiplication")
+    public void heUserGetsTheResultOfTheMultiplication() {
         try {
             LOGGER.info(new String(LastResponse.received().answeredBy(actor).asByteArray(), StandardCharsets.UTF_8));
             actor.should(
                     seeThatResponse("el codigo de respuesta es: " + HttpStatus.SC_OK,
                             response -> response.statusCode(HttpStatus.SC_OK)),
-                    seeThat(" la latitud y longitud es: ",
-                            responseSoap(), containsString("25.77,-80.2"))
+                    seeThat(" El resultado de la multiplicación es: ",
+                            responseSoap(), containsString("25"))
             );
             LOGGER.info("CUMPLE");
         } catch (Exception e) {
@@ -70,9 +71,9 @@ public class LatLongZipCodeStepDefinitions extends ApiSetUp {
             Assertions.fail();
         }
     }
-
     private void loadBody() {
-        body = readFile(BODY_PATH_ZIP_CODE.getValue());
-        body = String.format(body, "33101");
+        body = readFile(BODY_PATH_MULTIPLY.getValue());
+        body = String.format(body, "5","5");
     }
+
 }
