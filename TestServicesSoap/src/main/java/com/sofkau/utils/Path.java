35ba86b281c2db7230ebc8f839bcd2176dba8333
CristhianMorenoG <morenocristhian@outlook.com>
2023-03-27 15:48:38 -0500

    Se agrega primer feature de validacion de entidad emisora de tarjetas de credito

--- TestServicesSoap/src/main/java/com/sofkau/utils/Path.java
+++ TestServicesSoap/src/main/java/com/sofkau/utils/Path.java
@@ -2,8 +2,12 @@ package com.sofkau.utils;
 
 public enum Path {
     SOAP_CAPITAL_BASE_URL("http://webservices.oorsprong.org/"),
+    SOAP_CREDIT_CARD_VALIDATOR("https://secure.ftipgw.com/ArgoFire/"),
+    RESOURCE_CREDIT_CARD("validate.asmx?WSDL"),
+
     RESOURCE_CAPITAL("websamples.countryinfo/CountryInfoService.wso"),
-    BODY_PATH("src/test/resources/soap/filecapital.xml");
+    CAPITAL_BODY_PATH("src/test/resources/soap/filecapital.xml"),
+    CARD_BODY_PATH("src/test/resources/soap/filecreditcard.xml");
 
     private String value;
 
