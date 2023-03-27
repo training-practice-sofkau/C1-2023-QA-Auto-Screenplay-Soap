7d083fe855a10e1c3c1170909ecedcdf5d490ee5
Rasta <estiviti@hotmail.com>
2023-03-27 16:49:57 -0500

    Se arregla el escenario de multiplicar

--- TestServicesSoap/src/test/java/com/sofkau/stepdefinitions/MultiplicarStepDefinitions.java
+++ TestServicesSoap/src/test/java/com/sofkau/stepdefinitions/MultiplicarStepDefinitions.java
@@ -9,24 +9,9 @@ import net.serenitybdd.screenplay.rest.questions.LastResponse;
 import org.apache.http.HttpStatus;
 import org.apache.log4j.Logger;
 import org.junit.jupiter.api.Assertions;
-import org.w3c.dom.Node;
-import org.xml.sax.InputSource;
-import org.xml.sax.SAXException;
 
-import javax.swing.text.Document;
-import javax.xml.XMLConstants;
-import javax.xml.parsers.DocumentBuilder;
-import javax.xml.parsers.DocumentBuilderFactory;
-import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.transform.Transformer;
-import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.dom.DOMSource;
-import javax.xml.transform.stream.StreamResult;
-import java.io.IOException;
-import java.io.StringReader;
-import java.io.StringWriter;
 import java.nio.charset.StandardCharsets;
+
 
 import static com.sofkau.models.Headers.headers;
 import static com.sofkau.questions.ResponseSoap.responseSoap;
@@ -42,59 +27,9 @@ public class MultiplicarStepDefinitions extends ApiSetUp {
     private static final Logger LOGGER = Logger.getLogger(MultiplicarStepDefinitions.class);
     String body;
 
-    final static String str="<soapenv:Envelope xmlns:soapenv=\"http://schemas.xmlsoap.org/soap/envelope/\" xmlns:tem=\"http://tempuri.org/\">\n" +
-            "    <soapenv:Header/>\n" +
-            "    <soapenv:Body>\n" +
-            "        <tem:Multiply>\n" +
-            "            <tem:intA>%s</tem:intA>\n" +
-            "            <tem:intB>%s</tem:intB>\n" +
-            "        </tem:Multiply>\n" +
-            "    </soapenv:Body>\n" +
-            "</soapenv:Envelope>";
-
-
-
-
-    private static String convertXmlToString(Document doc) {
-        DOMSource domSource = new DOMSource((Node) doc);
-        StringWriter writer = new StringWriter();
-        StreamResult result = new StreamResult(writer);
-        TransformerFactory tf = TransformerFactory.newInstance();
-        Transformer transformer = null;
-        try {
-            transformer = tf.newTransformer();
-            transformer.transform(domSource, result);
-        } catch (TransformerException e) {
-            throw new RuntimeException(e);
-        }
-        return writer.toString();
-    }
-
-    private static Document convertStringToXml(String xmlString) {
-
-        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
-
-        try {
-
-            // optional, but recommended
-            // process XML securely, avoid attacks like XML External Entities (XXE)
-            dbf.setFeature(XMLConstants.FEATURE_SECURE_PROCESSING, true);
-
-            DocumentBuilder builder = dbf.newDocumentBuilder();
-
-            Document doc = (Document) builder.parse(new InputSource(new StringReader(xmlString)));
-
-            return doc;
-
-        } catch (ParserConfigurationException | IOException | SAXException e) {
-            throw new RuntimeException(e);
-        }
-
-    }
-
     private void loadBody(String num1,String num2) {
         body = readFile(BODY_MULTIPLICAR.getValue());
-        body = String.format(body, num1, num2);
+        body = String.format(body, Integer.parseInt(num1), Integer.parseInt(num2));
     }
     @Given("que estoy apuntando con un endpoint a la api de la calculadora")
     public void queEstoyApuntandoConUnEndpointALaApiDeLaCalculadora() {
@@ -110,14 +45,13 @@ public class MultiplicarStepDefinitions extends ApiSetUp {
 
     @When("envio la peticion post con el {string} y el {string}")
     public void envioLaPeticionPostConElYEl(String num1, String num2) {
-        Document document=convertStringToXml(String.format(str,num1,num2));
-        String xml = convertXmlToString(document);
+        loadBody(num1,num2);
         try{
 
             actor.attemptsTo(
                     doPostSoap()
                             .andTheResource(RESOURCE_CALCULADORA_MULTIPLICAR.getValue())
-                            .withTheHeaders(headers().getHeadersCollection())
+                            .withTheHeaders(headers().getHeadersCollectionCalculadora())
                             .andTheBody(body)
             );
         }catch (Exception e){
@@ -134,7 +68,7 @@ public class MultiplicarStepDefinitions extends ApiSetUp {
             actor.should(
                     seeThatResponse("el codigo de respuesta es: " + HttpStatus.SC_OK,
                             response -> response.statusCode(code)),
-                    seeThat(" la moneda es",
+                    seeThat(" El resultado es",
                             responseSoap(), containsString(resultado))
             );
 
--- TestServicesSoap/src/test/resources/features/multiplicar.feature
+++ TestServicesSoap/src/test/resources/features/multiplicar.feature
@@ -3,8 +3,13 @@ Feature: Multiplicar dos numero
   quiero relaizar peticiones al servicio post de multiplicar
   para obtener el resultado
 
-  @Moneda
-  Scenario: Obtener resultado de una multiplicacion
+  @Multiplicar
+  Scenario Outline: Obtener resultado de una multiplicacion
     Given que estoy apuntando con un endpoint a la api de la calculadora
     When envio la peticion post con el <num1> y el <num2>
     Then recibo <code> de codigo de respuesta y el <resultado> de la multiplicacion
+    Examples:
+      | num1        | num2    | code | resultado |
+      | "5"         | "1"     | 200  | "5"       |
+      | "1"         | "3"     | 200  | "3"       |
+      | "2"         | "4"     | 200  | "8"       |
